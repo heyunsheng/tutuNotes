b6494d09fce0eec0f42bd0a62e63cea0ae757395
贺赟生 <heyunsheng@jd.com>
2018-01-05 17:47:03 +0800

    添加工程

---
+++ TuTuNotes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TuTuNotes
+//
+//  Created by 贺赟生 on 2018/1/4.
+//  Copyright © 2018年 贺赟生. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
